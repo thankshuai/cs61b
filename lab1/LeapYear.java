6ecf73d5b5a95f6e743a809102a9c8d450b3781f
thankshuai <thankshuai@gmail.com>
2024-05-02 16:23:13 +0800

    Finished Lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,20 +1,15 @@
 import javax.swing.plaf.TreeUI;
 
 /** Class that determines whether or not a year is a leap year.
- *  @author zhangshuai
+ *  @author zhang shuai
  */
 public class LeapYear {
     /** determin if the given year is a leap year
      * @param year to be analyzed
      */
     public static boolean isLeapYear(int year){
-        if (year % 400 == 0){
-            return true;
-        }else if(year % 4 == 0 && year % 100 != 0){
-            return true;
-        }else{
-            return false;
-        }
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        
     }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
